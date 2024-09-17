9c7701fa92f84fe033b9874999f006022e97170a
sergei <adrian.illumina@gmail.com>
2024-09-18 02:27:23 +0700

    Added gamejoy, services and yetanothernet

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,3 +1,16 @@
+import { Context } from "@rbxts/gamejoy";
+import { Action } from "@rbxts/gamejoy/out/Actions";
 import { makeHello } from "shared/module";
 
 print(makeHello("main.client.ts"));
+
+const MainContext = new Context({
+	Process: false,
+	RunSynchronously: false,
+});
+
+const TestAction = new Action("F");
+
+MainContext.Bind(["F"], () => {
+	print("Keep Yourself Safe");
+});
